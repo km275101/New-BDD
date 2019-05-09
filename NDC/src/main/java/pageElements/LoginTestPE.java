0ba54c10040e9c9af64aaba7c730ce5b96a3fb08
kamlesh.maurya <kamlesh.maurya@PDC2A-9J0N6C2.dir.svc.accenture.com>
2019-05-09 17:14:47 +0530

    upadte

--- NDC/src/main/java/pageElements/LoginTestPE.java
+++ NDC/src/main/java/pageElements/LoginTestPE.java
@@ -1,9 +1,9 @@
 package pageElements;
 
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import coreAction.ActionDriver;
 
@@ -31,13 +31,5 @@ public class LoginTestPE extends ActionDriver{
 	@FindBy(xpath="//input[@type='submit']")
 	public WebElement submit ;
 	
-	/*
-	 * public void clickYourOrders() { try {
-	 * System.out.println("Click on your order button"); Thread.sleep(5000);
-	 * yourOrders.click(); } catch (Exception e) { // TODO Auto-generated catch
-	 * block e.printStackTrace(); }
-	 * 
-	 * }
-	 */
 
 }
--- NDC/src/test/java/defineStep/AmazonImplementation.java
+++ NDC/src/test/java/defineStep/AmazonImplementation.java
@@ -1,27 +1,28 @@
 package defineStep;
 
+
 import coreAction.Initialize;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import pageElements.LoginTestPE2;
+import junitRunner.Runner;
 import pageFunctions.LoginTestPF;
-
 public class AmazonImplementation extends Initialize{
 	
 	
 	@Given("^users open login page$")
 	public void users_open_login_page() throws Throwable {
+		setUp();
 		System.out.println("Opening amazon URL");
-	    //openUrl("https://www.amazon.in/");
+		System.out.println("Driver : "+driver);
+		openUrl("http://www.amazon.in");
 	}
 
 	@When("^user enters credentials$")
 	public void user_enters_credentials() throws Throwable {
 		
-		//lpf = new LoginTestPF(driver);
-		
+		lpf = new LoginTestPF(driver);
 		lpf.clickYourOrders();
 		lpf.setUsername("km275101@gmail.com");
 		lpf.clickLogin();
@@ -39,7 +40,6 @@ public class AmazonImplementation extends Initialize{
 	@Then("^user sees the login page$")
 	public void user_sees_the_login_page() throws Throwable {
 	    System.out.println("Successful login");
-	   closeBrowser();
 	}
 
 }
