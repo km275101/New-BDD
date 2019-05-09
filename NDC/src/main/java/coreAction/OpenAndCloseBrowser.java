f2cb4d2c3bda7c39f02d3e8253e9a78a67c78816
kamlesh.maurya <kamlesh.maurya@PDC2A-9J0N6C2.dir.svc.accenture.com>
2019-05-09 17:14:13 +0530

    core Action

--- NDC/src/main/java/coreAction/OpenAndCloseBrowser.java
+++ NDC/src/main/java/coreAction/OpenAndCloseBrowser.java
@@ -2,34 +2,43 @@ package coreAction;
 
 import java.util.concurrent.TimeUnit;
 
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-
-
 public class OpenAndCloseBrowser {
-	
+
 	public WebDriver driver;
-	
-	//@BeforeClass(alwaysRun = true)
-	public void openUrl(String url) throws InterruptedException{
-		 System.setProperty("webdriver.chrome.driver","C:\\Users\\kamlesh.maurya\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		 System.out.println("Opening chrome browser");
-		 driver = new ChromeDriver();
-		 driver.manage().window().maximize();
-		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		 System.out.println("Url is : "+url);
-     	 driver.get(url);
-		 System.out.println("Open and close browser class method --> Open Url");
-		 Thread.sleep(5000);
+
+	public WebDriver setUp() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\kamlesh.maurya\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.out.println("Opening chrome browser");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("OCB");
+		return driver;
 		
 	}
-	//@AfterClass
-	public void closeBrowser() throws InterruptedException{
+
+	// @BeforeClass(alwaysRun = true)
+	public WebDriver openUrl(String url) throws InterruptedException {
+		// System.setProperty("webdriver.chrome.driver","C:\\Users\\kamlesh.maurya\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		// System.out.println("Opening chrome browser");
+		// driver = new ChromeDriver();
+		// driver.manage().window().maximize();
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(url);
+		System.out.println("Open and close browser class method --> Open Url");
+		Thread.sleep(5000);
+		return driver;
+
+	}
+
+	@AfterClass
+	public void closeBrowser() throws InterruptedException {
 		driver.close();
 		driver.quit();
 		System.out.println("Closed the browser");
